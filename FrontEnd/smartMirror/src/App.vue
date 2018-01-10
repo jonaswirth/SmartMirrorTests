6d1f50a293a31ef21c3bf179c2ea4ba5be418224
Jonas Wirth <jonas-wirth@gmx.ch>
2018-01-10 17:06:17 +0100

    Setup of vue app

---
+++ FrontEnd/smartMirror/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div class="grid-row">
+      <div class="grid-box"></div>
+      <div class="grid-box"></div>
+      <div class="grid-box"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class App extends Vue {
+}
+</script>
+
+<style>
+body{
+  background-color: black;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
