0e5f6ea4738fb5961ea5d7e458b1da25a6de374b
Jonas Wirth <jonas-wirth@gmx.ch>
2018-01-10 17:49:08 +0100

    Added test controlls

--- FrontEnd/smartMirror/src/App.vue
+++ FrontEnd/smartMirror/src/App.vue
@@ -3,6 +3,9 @@
     <GridRow />
     <GridRow :isCommunicationRow="true" />
     <GridRow />
+    <div class="test-controlls">
+        <button>Say something</button>
+    </div>
   </div>
 </template>
 
@@ -36,26 +39,12 @@ html, body{
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  height: 100%;
+  height: 90%;
   padding:0;
   margin:0;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+div.test-controlls{
+  border: 1px solid white;
+  height: 10%;
 }
 </style>
